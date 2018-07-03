06ca81e9a4b1ee232559dd87062177ddf6007bbd
Eeps30 <eeps30@gmail.com>
2018-07-03 16:41:01 -0700

    changed files to be files from further in the class

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,42 +1,34 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-    if(err){
-        return console.log('Unable to connect to MongoDB server');
-    }
-    console.log('Connected to MongoDB server');
-    const db = client.db('TodoApp');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    // deleteMany (we have multiple todos in the DB that are the exact same 
-    //except for their unique id's)
-        // db.collection('Todos').deleteMany({text: 'Feed the frog'}).then((result) => {
-        //     console.log(result);
-        // })
+  // deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-    // deleteOne
-        // db.collection('Todos').deleteOne({text: 'Feed the frog'}).then((result) => {
-        //     console.log(result);
-        // })
+  // deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
 
-    // findOneAndDelete
-        // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
-        //     console.log(result);
-        // })
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
 
-    //class challenge: delete many with the same name, then findOneAndDelete by the id
-    //of that item in the collection
-        //delete one
-        // db.collection('Users').findOneAndDelete({
-        //     '_id': ObjectID('5b35a3bfc43b5421bc4bdd80')
-        // }).then((result) => {
-        //     console.log(result);
-        // })
+  // db.collection('Users').deleteMany({name: 'Andrew'});
 
-        //delete many
-        db.collection('Users').deleteMany({name: 'Evan'}).then((result) => {
-            console.log(result);
-        })
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID("57ac8d47878a299e5dc21bc8")
+  }).then((results) => {
+    console.log(JSON.stringify(results, undefined, 2));
+  });
 
-    // client.close();
-})
+  // db.close();
+});
--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,41 +1,38 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-    if(err){
-        return console.log('Unable to connect to MongoDB server');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
     }
-    console.log('Connected to MongoDB server');
-    const db = client.db('TodoApp');
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
 
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID('5b345caf2129e46ccd1c430b')
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // }, {
-    //     returnOriginal: false
-    // }).then((result) => {
-    //     console.log(result);
-    // })
-
-    //update the user in the user collection, setting it to something other than what it is
-    //currently AND increment its age by 1.
-    db.collection('Users').findOneAndUpdate({
-        _id: new ObjectID('5b35a3b6c43b5421bc4bdd7a')
-    }, {
-        $set: {
-            name: 'Smoothe'
-        },
-        $inc: {
-            age: 12
-        }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-        console.log(result);
-    });
-
-    // client.close();
-})
+  // db.close();
+});
